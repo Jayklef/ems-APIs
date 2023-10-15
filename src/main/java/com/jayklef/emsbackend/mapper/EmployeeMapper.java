e2c6715e8f601488076b88a740dd9133a3c0e570
Jayklef <jerryarhawho@gmail.com>
2023-10-15 22:15:59 +0100

    Employee entity and Dto Mapper

---
+++ src/main/java/com/jayklef/emsbackend/mapper/EmployeeMapper.java
@@ -0,0 +1,25 @@
+package com.jayklef.emsbackend.mapper;
+
+import com.jayklef.emsbackend.dto.EmployeeDto;
+import com.jayklef.emsbackend.entity.Employee;
+
+public class EmployeeMapper {
+
+    public static EmployeeDto mapToEmployeeDto(Employee employee){
+        return new EmployeeDto(
+                employee.getId(),
+                employee.getFirstName(),
+                employee.getLastName(),
+                employee.getEmail()
+        );
+    }
+
+    public static Employee mapToEmployee(EmployeeDto employeeDto){
+        return new Employee(
+                employeeDto.getId(),
+                employeeDto.getFirstname(),
+                employeeDto.getLastname(),
+                employeeDto.getEmail()
+        );
+    }
+}
